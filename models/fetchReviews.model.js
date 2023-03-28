509637278f56d37cef5ff751cd8e4b1a44f6fcec
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2023-03-28 18:42:23 +0100

    finished code review alterations Task 5

--- models/fetchReviews.model.js
+++ models/fetchReviews.model.js
@@ -1,10 +1,8 @@
 const db = require('../db/connection');
 
 function fetchReviews() {
-    return db.query(`SELECT reviews.*, CAST(COUNT(comments.review_id) AS INT) AS comment_count FROM reviews LEFT JOIN comments on reviews.review_id = comments.review_id GROUP BY reviews.review_id ORDER BY reviews.created_at DESC;`).then((review) => {
-        if (review.rows.length === 0) {
-            return Promise.reject({ status: 404, msg: "ID does not exist, please use a valid ID" })
-        }
+    return db.query(`SELECT reviews.review_id, reviews.title, reviews.category, reviews.designer, reviews.owner, reviews.review_img_url, reviews.created_at, reviews.votes, CAST(COUNT(comments.review_id) AS INT) AS comment_count FROM reviews LEFT JOIN comments on reviews.review_id = comments.review_id GROUP BY reviews.review_id ORDER BY reviews.created_at DESC;`).then((review) => {
+        console.log(review.rows)
         return review.rows
     });
 }
