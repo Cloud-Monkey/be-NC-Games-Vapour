dd24c41dcea1428a62b8cf47d3ade7bbbfe34b88
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2023-03-30 14:59:40 +0100

    changed env file

--- db/connection.js
+++ db/connection.js
@@ -5,9 +5,10 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
 }
+
 const config =
   ENV === 'production'
     ? {
